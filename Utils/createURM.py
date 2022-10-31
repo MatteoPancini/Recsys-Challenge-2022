63be387743a962b98e896aa5cbe2476ee10a7d12
MichaelVitali <michael.vitali@mail.polimi.it>
2022-10-31 19:03:29 +0100

    createURM + split functions

--- Utils/createURM.py
+++ Utils/createURM.py
@@ -1,1 +1,19 @@
-# TODO: fare funzione che crea URM
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+def createURM():
+
+    dataset = pd.read_csv('../Input/interactions_and_impressions.csv')
+    dataset = dataset.drop(columns=['Impressions'])
+
+    datasetCOO = sp.coo_matrix((dataset["Data"].values, (dataset["UserID"].values, dataset["ItemID"].values)))
+    userIDS = dataset['UserID'].unique()
+    itemIDS = dataset['ItemID'].unique()
+
+    URM = np.zeros((len(userIDS), len(itemIDS)), dtype=int)
+    for x in range(len(datasetCOO.data)):
+        if datasetCOO.data[x] == 0:
+            URM[datasetCOO.row[x]][datasetCOO.col[x]] = int(1)
+
+    URM = sp.csr_matrix(URM)
+    return URM
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 if __name__ == '__main__':
 
     from Recommenders.NonPersonalizedRecommender import TopPop
-    from Utils.loadURM import load_URM
     from Utils.writeSubmission import write_submission
     import pandas as pd
     import numpy as np
 
-    URM = load_URM("Input/interactions_and_impressions.csv")
+    #URM = load_URM("Input/interactions_and_impressions.csv")
 
     user_list = pd.read_csv('../Input/interactions_and_impressions.csv')['UserID'].unique().tolist()
     user_array = np.array(user_list)
