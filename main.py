1a59ef48f143c793dc457967bdb96eae69b107ec
MichaelVitali <michael.vitali@mail.polimi.it>
2022-11-01 10:19:05 +0100

    createURM path fix

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 if __name__ == '__main__':
+    from Utils.createURM import createURM
 
-    from Recommenders.NonPersonalizedRecommender import TopPop
-    from Utils.writeSubmission import write_submission
-    import pandas as pd
-    import numpy as np
-
-    #URM = load_URM("Input/interactions_and_impressions.csv")
-
-    user_list = pd.read_csv('../Input/interactions_and_impressions.csv')['UserID'].unique().tolist()
-    user_array = np.array(user_list)
-
-    topPopRecommender = TopPop(URM)
-    topPopRecommender.fit()
-    topPopRecommender.recommend(user_array)
-
-
-    write_submission(recommender=topPopRecommender, target_users_path="../Input/data_target_users_test.csv",
-                     out_path='../Output/{}_submission.csv'.format(topPopRecommender.RECOMMENDER_NAME))
+    URM = createURM()
+    print(URM)
