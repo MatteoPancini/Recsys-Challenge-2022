454babdb97d10aa98f91ee9e7e0ab4183126617f
MichaelVitali <michael.vitali@mail.polimi.it>
2022-11-06 10:17:44 +0100

    coarse-to-fine implementations

--- Main/ItemKNNCF.py
+++ Main/ItemKNNCF.py
@@ -1,15 +1,15 @@
 if __name__ == '__main__':
 
-    from Utils.createURM import createURM
+    from Utils.createURM import createURM2
     from Utils.writeSubmission import write_submission
     from Recommenders.KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
 
-    URM = createURM()
+    URM = createURM2()
 
     recommender = ItemKNNCFRecommender(URM)
     # BEST recommender.fit(shrink = int(11.360087017080575), topK = int(24.286589663434658))
 
-    recommender.fit(shrink = int(24.222108288044993), topK = int(51.671247960899215))
+    recommender.fit(shrink = 58.04637977082229, topK = 146)
 
     write_submission(recommender=recommender,
                      target_users_path="../Input/data_target_users_test.csv",
--- Utils/createURM.py
+++ Utils/createURM.py
@@ -31,8 +31,29 @@ def newCreateURM():
     newURM = np.zeros((len(userIDS), 27968), dtype=int)
     for x in range(len(datasetCOO.data)):
         if datasetCOO.data[x] == 0:
+            newURM[datasetCOO.row[x]][datasetCOO.col[x]] = int(5)
+        elif newURM[datasetCOO.row[x]][datasetCOO.col[x]] != int(5) and datasetCOO.data[x] == 1:
             newURM[datasetCOO.row[x]][datasetCOO.col[x]] = int(1)
 
     newURM = sp.csr_matrix(newURM)
 
     return newURM
+
+def createURM2():
+    dataset = pd.read_csv('../Input/interactions_and_impressions.csv')
+    dataset = dataset.drop(columns=['Impressions'])
+
+    datasetCOO = sp.coo_matrix((dataset["Data"].values, (dataset["UserID"].values, dataset["ItemID"].values)))
+    userIDS = dataset['UserID'].unique()
+    itemIDS = dataset['ItemID'].unique()
+
+    URM = np.zeros((len(userIDS), len(itemIDS)), dtype=int)
+    for x in range(len(datasetCOO.data)):
+        if datasetCOO.data[x] == 0:
+            URM[datasetCOO.row[x]][datasetCOO.col[x]] = int(5)
+        elif URM[datasetCOO.row[x]][datasetCOO.col[x]] != int(5) and datasetCOO.data[x] == 1:
+            URM[datasetCOO.row[x]][datasetCOO.col[x]] = int(1)
+
+    URM = sp.csr_matrix(URM)
+
+    return URM
