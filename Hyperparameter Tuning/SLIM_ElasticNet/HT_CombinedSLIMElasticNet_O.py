03ab34966b9b4a4df80cddb5cb63c14d78013161
matteopancini <matteo.pancini@mail.polimi.it>
2022-11-28 18:40:44 +0100

    Commit

--- Hyperparameter Tuning/SLIM_ElasticNet/HT_CombinedSLIMElasticNet_O.py
+++ Hyperparameter Tuning/SLIM_ElasticNet/HT_CombinedSLIMElasticNet_O.py
@@ -2,20 +2,36 @@ if __name__ == "__main__":
 
     from Recommenders.SLIM.SLIMElasticNetRecommender import MultiThreadSLIM_SLIMElasticNetRecommender
     from Evaluation.K_Fold_Evaluator import K_Fold_Evaluator_MAP
-    from Utils.recsys2022DataReader import createBigBumpURM, createBigICM
+    from Utils.recsys2022DataReader import *
     from Data_manager.split_functions.split_train_validation_random_holdout import split_train_in_two_percentage_global_sample
     from Evaluation.Evaluator import EvaluatorHoldout
     import json
     from datetime import datetime
     import optuna as op
     import scipy.sparse as sps
+    import similaripy
+    import csv
 
     # ---------------------------------------------------------------------------------------------------------
-    # Loading URM
-    URM = createBigBumpURM()
-    ICM = createBigICM()
+    # Loading URM & ICM
+    URM = createURM()
 
-    ICM.data = ICM.data * 0.1
+    ICM = createSmallICM()
+
+    ICM = similaripy.normalization.bm25plus(ICM.copy())
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Creating CSV header
+
+    header = ['recommender', 'topK', 'alpha', 'l1_ration', 'ICMweight', 'MAP']
+
+    partialsFile = 'Combinedpartials_' + datetime.now().strftime('%b%d_%H-%M-%S')
+
+    with open('partials/' + partialsFile + '.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
 
 
     # ---------------------------------------------------------------------------------------------------------
@@ -28,15 +44,14 @@ if __name__ == "__main__":
 
     for k in range(3):
         URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train_init, train_percentage=0.85)
-        URM_train = URM_train * 0.9
         URM_train_list.append(URM_train)
         URM_validation_list.append(URM_validation)
 
     evaluator_validation = K_Fold_Evaluator_MAP(URM_validation_list, cutoff_list=[10], verbose=False)
 
-    ICMs_combined = []
+    CombinedURMs = []
     for URM in URM_train_list:
-        ICMs_combined.append(sps.vstack([URM, ICM.T]))
+        CombinedURMs.append(sps.vstack([URM, ICM.T]))
 
     MAP_results_list = []
 
@@ -50,20 +65,29 @@ if __name__ == "__main__":
         topK = trial.suggest_int("topK", 100, 500)
         alpha = trial.suggest_float("alpha", 1e-5, 0.1)
         l1_ratio = trial.suggest_float("l1_ratio", 1e-5, 0.1)
+        ICMweight = trial.suggest_int("icm_weight", 1, 100)
 
         for index in range(len(URM_train_list)):
 
-            recommender_SlimElasticnet_list.append(MultiThreadSLIM_SLIMElasticNetRecommender(ICMs_combined[index]))
+            CombinedURM = sps.vstack([URM_train_list[index], (ICM * ICMweight).T])
+            recommender_SlimElasticnet_list.append(MultiThreadSLIM_SLIMElasticNetRecommender(CombinedURM))
             recommender_SlimElasticnet_list[index].fit(alpha=alpha, l1_ratio=l1_ratio, topK=topK)
 
         MAP_result = evaluator_validation.evaluateRecommender(recommender_SlimElasticnet_list)
         MAP_results_list.append(MAP_result)
 
+        resultsToPrint = [recommender_SlimElasticnet_list[0].RECOMMENDER_NAME, topK, alpha, l1_ratio, ICMweight,
+                          sum(MAP_result) / len(MAP_result)]
+
+        with open('partials/' + partialsFile + '.csv', 'a+', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(resultsToPrint)
+
         return sum(MAP_result) / len(MAP_result)
 
 
     study = op.create_study(direction='maximize')
-    study.optimize(objective, n_trials=5)
+    study.optimize(objective, n_trials=20)
 
     # ---------------------------------------------------------------------------------------------------------
     # Fitting and testing to get local MAP
@@ -71,8 +95,11 @@ if __name__ == "__main__":
     topK = study.best_params["topK"]
     alpha = study.best_params["alpha"]
     l1_ratio = study.best_params["l1_ratio"]
+    ICMweight = study.best_params("icm_weight")
 
-    recommender_SlimElasticnet = MultiThreadSLIM_SLIMElasticNetRecommender(URM_train_init)
+    CombinedURM = sps.vstack([URM_train_init, (ICM * ICMweight).T])
+
+    recommender_SlimElasticnet = MultiThreadSLIM_SLIMElasticNetRecommender(CombinedURM)
     recommender_SlimElasticnet.fit(alpha=alpha, l1_ratio=l1_ratio, topK=topK)
 
     evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10])
@@ -84,7 +111,7 @@ if __name__ == "__main__":
     resultParameters = result_dict.to_json(orient="records")
     parsed = json.loads(resultParameters)
 
-    with open("logs/" + recommender_SlimElasticnet.RECOMMENDER_NAME + "_logs_" + datetime.now().strftime(
+    with open("logs/Combined" + recommender_SlimElasticnet.RECOMMENDER_NAME + "_logs_" + datetime.now().strftime(
             '%b%d_%H-%M-%S') + ".json", 'w') as json_file:
         json.dump(study.best_params, json_file, indent=4)
         json.dump(parsed, json_file, indent=4)
--- Recommenders/Implicit/ImplicitALSRecommender.py
+++ Recommenders/Implicit/ImplicitALSRecommender.py
@@ -1,3 +1,5 @@
+import os
+
 import implicit
 from ..BaseMatrixFactorizationRecommender import BaseMatrixFactorizationRecommender
 from ..Recommender_utils import check_matrix
@@ -7,6 +9,8 @@ class ImplicitALSRecommender(BaseMatrixFactorizationRecommender):
     """ImplicitALSRecommender recommender"""
 
     RECOMMENDER_NAME = "ImplicitALSRecommender"
+
+    os.environ['MKL_NUM_THREADS'] = '1'
 
     def fit(self,
             factors=100,
@@ -18,18 +22,33 @@ class ImplicitALSRecommender(BaseMatrixFactorizationRecommender):
             alpha=24
             ):
 
+        print('inizio fit')
+        os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+
+        sparse_item_user = self.URM_train.transpose()
+
+
         self.rec = implicit.als.AlternatingLeastSquares(factors=factors, regularization=regularization,
                                                         use_native=use_native, use_cg=use_cg, use_gpu=use_gpu,
                                                         iterations=iterations,
                                                         calculate_training_loss=calculate_training_loss,
                                                         num_threads=num_threads)
+        print('check matrix')
 
-        C = check_matrix(self.URM_train, format="csr", dtype=np.float32)
+        C = check_matrix(sparse_item_user, format="csr", dtype=np.float32)
         C.data = 1.0 + alpha * C.data
 
+        print(C.shape)
+
         self.rec.fit(C, show_progress=self.verbose)
+        print('non arrivo qua')
 
         self.USER_factors = self.rec.user_factors
         self.ITEM_factors = self.rec.item_factors
+
+        print(self.rec.user_factors.shape)
+        print(self.rec.item_factors.shape)
+
 
 
