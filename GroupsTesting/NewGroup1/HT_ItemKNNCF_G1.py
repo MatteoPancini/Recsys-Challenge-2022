4b524c8b1bb6ddfad9a6da6d8fe20f6fa516aff1
MichaelVitali <michael.vitali@mail.polimi.it>
2022-12-06 21:38:26 +0100

    new Groups

---
+++ GroupsTesting/NewGroup1/HT_ItemKNNCF_G1.py
@@ -0,0 +1,122 @@
+if __name__ == '__main__':
+
+    from Evaluation.Evaluator import EvaluatorHoldout
+    from Evaluation.K_Fold_Evaluator import K_Fold_Evaluator_MAP
+    from datetime import datetime
+    from Utils.recsys2022DataReader import *
+    from Data_manager.split_functions.split_train_validation_random_holdout import \
+        split_train_in_two_percentage_global_sample
+    from Recommenders.KNN.ItemKNNCFRecommenderPLUS import ItemKNNCFRecommender
+    import optuna as op
+    import json
+    import csv
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Loading URM & ICM
+
+    URM = createURM()
+    ICM = createSmallICM()
+
+    URM_train_init, URM_test = split_train_in_two_percentage_global_sample(URM, train_percentage=0.85)
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Creating CSV header
+
+    header = ['recommender', 'shrink', 'topk', 'similarity', 'normalization',  'MAP']
+    partialsFile = 'CombinedItemKNNCF_' + datetime.now().strftime('%b%d_%H-%M-%S')
+
+    with open('partials/' + partialsFile + '.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        writer.writerow(header)
+
+    # ---------------------------------------------------------------------------------------------------------
+    # K-Fold Cross Validation + Preparing training, validation, test split and evaluator
+
+    URM_train_list = []
+    URM_validation_list = []
+    users_not_in_group_list = []
+
+    interactions = []
+    for i in range(41629):
+        interactions.append(len(URM[i, :].nonzero()[0]))
+
+    for k in range(5):
+        URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train_init, train_percentage=0.85)
+        URM_train_list.append(URM_train)
+        URM_validation_list.append(URM_validation)
+
+        profile_length = np.ediff1d(URM.indptr)
+        sorted_users = np.argsort(profile_length)
+
+        lower_bound = 23
+        higher_bound = 35
+
+        users_in_group = [user_id for user_id in range(len(interactions)) if
+                          (lower_bound <= interactions[user_id] <= higher_bound)]
+        users_in_group_p_len = profile_length[users_in_group]
+
+        users_not_in_group_flag = np.isin(sorted_users, users_in_group, invert=True)
+        users_not_in_group_list.append(sorted_users[users_not_in_group_flag])
+
+    evaluator_validation = K_Fold_Evaluator_MAP(URM_validation_list, cutoff_list=[10], verbose=False, ignore_users_list=users_not_in_group_list)
+
+    MAP_results_list = []
+    # ---------------------------------------------------------------------------------------------------------
+    # Optuna hyperparameter model
+
+    def objective(trial):
+
+        recommender_ItemKNNCF_list = []
+
+        topK = trial.suggest_int("topK", 100, 500)
+        shrink = trial.suggest_float("shrink", 10, 200)
+        similarity = trial.suggest_categorical("similarity", ["cosine", "rp3beta"])
+        normalization = trial.suggest_categorical("normalization", ["bm25", "tfidf", "bm25plus"])
+
+        for index in range(len(URM_train_list)):
+
+            recommender_ItemKNNCF_list.append(ItemKNNCFRecommender(URM_train_list[index], verbose=False))
+            recommender_ItemKNNCF_list[index].fit(ICM=ICM, shrink=shrink, topK=topK, similarity=similarity,
+                                                  normalization=normalization)
+
+        MAP_result = evaluator_validation.evaluateRecommender(recommender_ItemKNNCF_list)
+        MAP_results_list.append(MAP_result)
+
+        resultsToPrint = [recommender_ItemKNNCF_list[0].RECOMMENDER_NAME, shrink, topK, similarity, normalization, sum(MAP_result) / len(MAP_result)]
+
+        with open('partials/' + partialsFile + '.csv', 'a+', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            writer.writerow(resultsToPrint)
+
+        return sum(MAP_result) / len(MAP_result)
+
+
+    study = op.create_study(direction='maximize')
+    study.optimize(objective, n_trials=100)
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Fitting and testing to get local MAP
+
+    topK = study.best_params['topK']
+    shrink = study.best_params['shrink']
+    similarity = study.best_params['similarity']
+    normalization = study.best_params['normalization']
+
+    recommender_ItemKNNCF = ItemKNNCFRecommender(URM_train_init, verbose=False)
+    recommender_ItemKNNCF.fit(ICM=ICM, shrink=shrink, topK=topK, similarity=similarity, normalization=normalization)
+
+    evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10], ignore_users=users_not_in_group)
+    result_dict, _ = evaluator_test.evaluateRecommender(recommender_ItemKNNCF)
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Writing hyperparameter into a log
+
+    resultParameters = result_dict.to_json(orient="records")
+    parsed = json.loads(resultParameters)
+
+    with open("logs/" + recommender_ItemKNNCF.RECOMMENDER_NAME + "_logs_" + datetime.now().strftime(
+            '%b%d_%H-%M-%S') + ".json", 'w') as json_file:
+        json.dump(study.best_params, json_file, indent=4)
+        json.dump(parsed, json_file, indent=4)
---
+++ GroupsTesting/NewGroup2/Group2.py
@@ -0,0 +1,110 @@
+if __name__ == "__main__":
+
+    import scipy.sparse as sp
+    from Data_manager.split_functions.split_train_validation_random_holdout import \
+        split_train_in_two_percentage_global_sample
+    from Utils.recsys2022DataReader import *
+    from Recommenders.Hybrid.LinearHybridRecommender import LinearHybridTwoRecommenderTwoVariables
+    from Recommenders.KNN.ItemKNNCFRecommenderPLUS import ItemKNNCFRecommender
+    from Recommenders.SLIM.SLIMElasticNetRecommender import MultiThreadSLIM_SLIMElasticNetRecommender
+    from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
+    import matplotlib.pyplot as plt
+    from Evaluation.Evaluator import EvaluatorHoldout
+    from datetime import datetime
+    import json
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Loading URM + ICM
+
+    URM = createURM()
+    ICM = createSmallICM()
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Preparing training, validation, test split and evaluator
+
+    URM_train, URM_test = split_train_in_two_percentage_global_sample(URM, train_percentage=0.85)
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Fitting of recommenders
+
+    recommender_object_dict = {}
+    """
+    # SLIM Elastic Net
+    SlimElasticNet = MultiThreadSLIM_SLIMElasticNetRecommender(URM_train)
+    SlimElasticNet.fit(topK=359, alpha=0.04183472018614359, l1_ratio=0.03260349571135893)
+    recommender_object_dict['SLIM Elastic Net'] = SlimElasticNet"""
+
+    # ItemKNNCF
+    ItemKNNCFG3 = ItemKNNCFRecommender(URM_train)
+    ItemKNNCFG3.fit(ICM, shrink=57.6924228938274, topK=408, similarity='dice', normalization='bm25')
+    recommender_object_dict['CombinedItemKNNCFG2'] = ItemKNNCFG3
+
+    # RP3beta
+    RP3betaG3 = RP3betaRecommender(URM_train)
+    RP3betaG3.fit(alpha=0.5674554399991163, beta=0.38051048617892586, topK=100)
+    recommender_object_dict['RP3betaG2'] = RP3betaG3
+
+    # RP3beta_ItemKNN Hybrid
+    recommender1 = ItemKNNCFRecommender(URM_train)
+    recommender1.fit(ICM, shrink=57.6924228938274, topK=408, similarity='dice', normalization='bm25')
+
+    recommender2 = RP3betaRecommender(URM_train)
+    recommender2.fit(alpha=0.5674554399991163, beta=0.38051048617892586, topK=100)
+
+    hybrid = LinearHybridTwoRecommenderTwoVariables(URM_train, recommender1, recommender2)
+    hybrid.fit(alpha=0.00793735238105765, beta=0.24158612307881616)
+    recommender_object_dict['RP3beta_ItemKNNCF_Hybrid'] = hybrid
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Evaluation of recommenders based on group
+
+    MAP_recommender_per_group = {}
+
+    cutoff = 10
+
+    profile_length = np.ediff1d(URM.indptr)
+    sorted_users = np.argsort(profile_length)
+
+    interactions = []
+    for i in range(41629):
+        interactions.append(len(URM[i, :].nonzero()[0]))
+
+    lower_bound = 36
+    higher_bound = max(interactions)
+
+    users_in_group = [user_id for user_id in range(len(interactions)) if
+                      (lower_bound <= interactions[user_id] <= higher_bound)]
+    users_in_group_p_len = profile_length[users_in_group]
+
+    users_not_in_group_flag = np.isin(sorted_users, users_in_group, invert=True)
+    users_not_in_group = sorted_users[users_not_in_group_flag]
+
+    evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[cutoff], ignore_users=users_not_in_group)
+
+    for label, recommender in recommender_object_dict.items():
+        result_df, _ = evaluator_test.evaluateRecommender(recommender)
+        if label in MAP_recommender_per_group:
+            MAP_recommender_per_group[label].append(result_df.loc[cutoff]["MAP"])
+        else:
+            MAP_recommender_per_group[label] = [result_df.loc[cutoff]["MAP"]]
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Plot
+
+    finalResults = {}
+    _ = plt.figure(figsize=(16, 9))
+    for label, recommender in recommender_object_dict.items():
+        results = MAP_recommender_per_group[label]
+        finalResults[label] = results
+        plt.scatter(x=label, y=results, label=label)
+    plt.title('User Group 3')
+    plt.ylabel('MAP')
+    plt.xlabel('Recommenders')
+    plt.legend()
+    plt.show()
+
+
+    with open("logs/Group3_logs_" + datetime.now().strftime(
+            '%b%d_%H-%M-%S') + ".json", 'w') as json_file:
+        json.dump(finalResults, json_file, indent=4)
+
