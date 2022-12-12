c47a5821e5cafa16c9439905b22bf72b1460d981
MichaelVitali <michael.vitali@mail.polimi.it>
2022-12-12 17:16:58 +0100

    Updates Hybrids

--- NewGroups/Cold/HT_RP3beta_ItemKNNCF_Cold.py
+++ NewGroups/Cold/HT_RP3beta_ItemKNNCF_Cold.py
@@ -1,37 +1,38 @@
 if __name__ == "__main__":
-
     from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
+    from Recommenders.KNN.ItemKNNCFRecommenderPLUS import ItemKNNCFRecommender
     from Evaluation.K_Fold_Evaluator import K_Fold_Evaluator_MAP
-    from Utils.recsys2022DataReader import createURM
-    from Data_manager.split_functions.split_train_validation_random_holdout import split_train_in_two_percentage_global_sample
+    from Recommenders.Hybrid.LinearHybridRecommender import LinearHybridTwoRecommenderOneVariable
+    from Utils.recsys2022DataReader import createURM, createSmallICM
+    from Data_manager.split_functions.split_train_validation_random_holdout import \
+        split_train_in_two_percentage_global_sample
     from Evaluation.Evaluator import EvaluatorHoldout
-    import json
-    from datetime import datetime
-    import optuna as op
     import numpy as np
     import csv
-    from optuna.samplers import RandomSampler, GridSampler
+    from datetime import datetime
+    import optuna as op
+    import json
+    from optuna.samplers import RandomSampler
 
     # ---------------------------------------------------------------------------------------------------------
     # Loading URM
     URM = createURM()
+    ICM = createSmallICM()
 
     URM_train_init, URM_test = split_train_in_two_percentage_global_sample(URM, train_percentage=0.85)
-
 
     # ---------------------------------------------------------------------------------------------------------
     # Creating CSV header
 
-    header = ['recommender', 'alpha', 'beta', 'TopK', 'MAP']
+    header = ['recommender', 'alpha', 'MAP']
 
-    partialsFile = 'RP3beta_' + datetime.now().strftime('%b%d_%H-%M-%S')
+    partialsFile = 'RP3beta_ItemKNNCF_' + datetime.now().strftime('%b%d_%H-%M-%S')
 
     with open('partials/' + partialsFile + '.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
 
         # write the header
         writer.writerow(header)
-
 
     # ---------------------------------------------------------------------------------------------------------
     # Profiling
@@ -64,7 +65,7 @@ if __name__ == "__main__":
     URM_validation_list = []
     users_not_in_group_list = []
 
-    for k in range(5):
+    for k in range(3):
         URM_train, URM_validation = split_train_in_two_percentage_global_sample(URM_train_init, train_percentage=0.85)
         URM_train_list.append(URM_train)
         URM_validation_list.append(URM_validation)
@@ -87,23 +88,31 @@ if __name__ == "__main__":
     # ---------------------------------------------------------------------------------------------------------
     # Optuna hyperparameter model
 
+    recommender_RP3beta_list = []
+    recommender_ItemKNN_list = []
+
+    for index in range(len(URM_train_list)):
+
+        recommender_RP3beta_list.append(RP3betaRecommender(URM_train_list[index], verbose=False))
+        recommender_RP3beta_list[index].fit(alpha=0.6627101454340679, beta=0.2350020032542621, topK=250)
+
+        recommender_ItemKNN_list.append(ItemKNNCFRecommender(URM_train_list[index], verbose=False))
+        recommender_ItemKNN_list[index].fit(ICM=ICM, topK=5893, shrink=50, similarity='rp3beta', normalization='tfidf')
+
     def objective(trial):
 
-        recommender_RP3beta_list = []
-
+        recommender_hybrid_list = []
         alpha = trial.suggest_float("alpha", 0.1, 0.9)
-        beta = trial.suggest_float("beta", 0.1, 0.9)
-        topK = trial.suggest_int("topK", 5, 1000)
 
         for index in range(len(URM_train_list)):
-            recommender_RP3beta_list.append(RP3betaRecommender(URM_train_list[index], verbose=False))
-            recommender_RP3beta_list[index].fit(alpha=alpha, topK=topK, beta=beta)
 
-        MAP_result = evaluator_validation.evaluateRecommender(recommender_RP3beta_list)
+            recommender_hybrid_list.append(LinearHybridTwoRecommenderOneVariable(URM_train_list[index], Recommender_1=recommender_RP3beta_list[index], Recommender_2=recommender_ItemKNN_list[index]))
+            recommender_hybrid_list[index].fit(alpha=alpha)
+
+        MAP_result = evaluator_validation.evaluateRecommender(recommender_hybrid_list)
         MAP_results_list.append(MAP_result)
 
-        resultsToPrint = [recommender_RP3beta_list[0].RECOMMENDER_NAME, alpha, beta, topK,
-                          sum(MAP_result) / len(MAP_result)]
+        resultsToPrint = ["RP3beta_ItemKNNCF", alpha, sum(MAP_result) / len(MAP_result)]
 
         with open('partials/' + partialsFile + '.csv', 'a+', encoding='UTF8') as f:
             writer = csv.writer(f)
@@ -111,21 +120,26 @@ if __name__ == "__main__":
 
         return sum(MAP_result) / len(MAP_result)
 
+
     study = op.create_study(direction='maximize', sampler=RandomSampler())
-    study.optimize(objective, n_trials=150)
+    study.optimize(objective, n_trials=100)
 
     # ---------------------------------------------------------------------------------------------------------
     # Fitting and testing to get local MAP
 
-    topK = study.best_params['topK']
     alpha = study.best_params['alpha']
-    beta = study.best_params['beta']
 
     recommender_RP3beta = RP3betaRecommender(URM_train_init, verbose=False)
-    recommender_RP3beta.fit(alpha=alpha, beta=beta, topK=topK)
+    recommender_RP3beta.fit(alpha=0.6627101454340679, beta=0.2350020032542621, topK=250)
+
+    recommender_ItemKNN = ItemKNNCFRecommender(URM_train_init, verbose=False)
+    recommender_ItemKNN.fit(ICM=ICM, topK=5893, shrink=50, similarity='rp3beta', normalization='tfidf')
+
+    recommender_hybrid = LinearHybridTwoRecommenderOneVariable(URM_train_init, Recommender_1=recommender_RP3beta, Recommender_2=recommender_ItemKNN)
+    recommender_hybrid.fit(alpha=alpha)
 
     evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10], ignore_users=users_not_in_group)
-    result_dict, _ = evaluator_test.evaluateRecommender(recommender_RP3beta)
+    result_dict, _ = evaluator_test.evaluateRecommender(recommender_hybrid)
 
     # ---------------------------------------------------------------------------------------------------------
     # Writing hyperparameter into a log
@@ -133,7 +147,8 @@ if __name__ == "__main__":
     resultParameters = result_dict.to_json(orient="records")
     parsed = json.loads(resultParameters)
 
-    with open("logs/" + recommender_RP3beta.RECOMMENDER_NAME + "_logs_" + datetime.now().strftime(
+    with open("logs/" + "RP3beta_ItemKNNCF" + "_logs_" + datetime.now().strftime(
             '%b%d_%H-%M-%S') + ".json", 'w') as json_file:
         json.dump(study.best_params, json_file, indent=4)
         json.dump(parsed, json_file, indent=4)
+
