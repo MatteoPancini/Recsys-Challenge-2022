162fce9dc96e6d9d9201b81ed8a60879086d11f7
matteopancini <matteo.pancini@mail.polimi.it>
2022-12-15 17:39:28 +0100

    NEW HYPERPARAM

--- Utils/recsys2022DataReader.py
+++ Utils/recsys2022DataReader.py
@@ -6,15 +6,17 @@ import numpy as np
 # - ../../Input/ if run from notebooks and main Hybrid .py
 # - ../../../Input/ if run from python file
 
-urmPath = "Input/interactions_and_impressions.csv"
-icmTypePath = "Input/data_ICM_type.csv"
-icmLenghtPath = "Input/data_ICM_length.csv"
+urmPath = "../../Input/interactions_and_impressions.csv"
+icmTypePath = "../../Input/data_ICM_type.csv"
+icmLenghtPath = "../../Input/data_ICM_length.csv"
 '''
 urmPath = "../../Input/interactions_and_impressions.csv"
 icmTypePath = "../../Input/data_ICM_type.csv"
 icmLenghtPath = "../../Input/data_ICM_length.csv"
 '''
 targetUserPath = "../../../Input/data_target_users_test.csv"
+
+sourceDataset = '../../Dataset/'
 
 def createURM():
     dataset = pd.read_csv(urmPath)
@@ -202,3 +204,30 @@ def createBigICM():
     ICM = sp.csr_matrix(ICM)
 
     return ICM
+
+def load_URMTrainInit():
+    URM_train_init = sp.load_npz(sourceDataset+'URM_train_init.npz')
+    return URM_train_init
+
+def load_URMTest():
+    URM_test = sp.load_npz(sourceDataset + 'URM_test.npz')
+    return URM_test
+
+
+def load_K_URMTrain():
+    URM_train_list = []
+
+    URM_train_list.append(sp.load_npz(sourceDataset + 'URM_train0.npz'))
+    URM_train_list.append(sp.load_npz(sourceDataset + 'URM_train1.npz'))
+    URM_train_list.append(sp.load_npz(sourceDataset + 'URM_train2.npz'))
+
+    return URM_train_list
+
+def load_K_URMValid():
+    URM_val_list = []
+
+    URM_val_list.append(sp.load_npz(sourceDataset + 'URM_val0.npz'))
+    URM_val_list.append(sp.load_npz(sourceDataset + 'URM_val1.npz'))
+    URM_val_list.append(sp.load_npz(sourceDataset + 'URM_val2.npz'))
+
+    return URM_val_list
