21e98ed462fcef47a6a7b1d540d04cc254000bdc
MichaelVitali <michael.vitali@mail.polimi.it>
2022-12-16 09:01:14 +0100

    tuning hybrid cold

--- Hyperparameter Tuning New/Hybrid/HT_COLDItemKNN-RP3beta.py
+++ Hyperparameter Tuning New/Hybrid/HT_COLDItemKNN-RP3beta.py
@@ -36,7 +36,37 @@ if __name__ == '__main__':
     URM_test = load_URMTest()
     ICM = createSmallICM()
 
-    evaluator_validation = K_Fold_Evaluator_MAP(URM_validation_list, cutoff_list=[10], verbose=False)
+    # ---------------------------------------------------------------------------------------------------------
+    # Profiling
+
+    group_id = 0
+
+    cutoff = 10
+
+    profile_length = np.ediff1d(URM_train_init.indptr)
+    sorted_users = np.argsort(profile_length)
+
+    interactions = []
+    for i in range(41629):
+        interactions.append(len(URM_train_init[i, :].nonzero()[0]))
+
+    list_group_interactions = [[0, 20], [21, 49], [50, max(interactions)]]
+
+    lower_bound = list_group_interactions[group_id][0]
+    higher_bound = list_group_interactions[group_id][1]
+
+    users_in_group = [user_id for user_id in range(len(interactions))
+                      if (lower_bound <= interactions[user_id] <= higher_bound)]
+    users_in_group_p_len = profile_length[users_in_group]
+
+    users_not_in_group_flag = np.isin(sorted_users, users_in_group, invert=True)
+    users_not_in_group = sorted_users[users_not_in_group_flag]
+
+    user_not_in_group_list = []
+    for i in range(3):
+        user_not_in_group_list.append(users_not_in_group)
+
+    evaluator_validation = K_Fold_Evaluator_MAP(URM_validation_list, cutoff_list=[10], verbose=False, ignore_users_list=user_not_in_group_list)
 
     MAP_results_list = []
 
@@ -49,10 +79,10 @@ if __name__ == '__main__':
 
     for index in range(len(URM_train_list)):
         recommender_ItemKNN_list.append(ItemKNNCFRecommender(URM_train_list[index]))
-        recommender_ItemKNN_list[index].fit(ICM, shrink=1665.2431108249625, topK=3228, similarity='dice', normalization='bm25')
+        recommender_ItemKNN_list[index].fit(ICM, shrink=92, topK=1995, similarity='rp3beta', normalization='tfidf')
 
         recommender_RP3beta_list.append(RP3betaRecommender(URM_train_list[index]))
-        recommender_RP3beta_list[index].fit(alpha=0.748706443270007, beta=0.16081149387492433, topK=370)
+        recommender_RP3beta_list[index].fit(alpha=0.4939469719298136, beta=0.31172220009612456, topK=423)
 
 
     def objective(trial):
@@ -79,7 +109,7 @@ if __name__ == '__main__':
 
 
     study = op.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=600)
 
     # ---------------------------------------------------------------------------------------------------------
     # Fitting and testing to get local MAP
@@ -88,15 +118,15 @@ if __name__ == '__main__':
     beta = study.best_params['beta']
 
     rec1 = ItemKNNCFRecommender(URM_train_init)
-    rec1.fit(ICM, shrink=1665.2431108249625, topK=3228, similarity='dice', normalization='bm25')
+    rec1.fit(ICM, shrink=92, topK=1995, similarity='rp3beta', normalization='tfidf')
 
     rec2 = RP3betaRecommender(URM_train_init)
-    rec2.fit(alpha=0.748706443270007, beta=0.16081149387492433, topK=370)
+    rec2.fit(alpha=0.4939469719298136, beta=0.31172220009612456, topK=423)
 
-    hybrid = LinearHybridTwoRecommenderNoVariables(URM_train=URM_train_init, Recommender_1=rec1, Recommender_2=rec2)
+    hybrid = LinearHybridTwoRecommenderTwoVariables(URM_train=URM_train_init, Recommender_1=rec1, Recommender_2=rec2)
     hybrid.fit(alpha=alpha, beta=beta)
 
-    evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10])
+    evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10], ignore_users=users_not_in_group)
     result_dict, _ = evaluator_test.evaluateRecommender(hybrid)
 
     # ---------------------------------------------------------------------------------------------------------
