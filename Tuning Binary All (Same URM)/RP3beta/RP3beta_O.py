e68564e08d0fe73c24d7c5c693bde3beadaabcbb
MichaelVitali <michael.vitali@mail.polimi.it>
2022-12-30 14:25:51 +0100

    tuning binary RP3beta

--- Tuning Binary All (Same URM)/RP3beta/RP3beta_O.py
+++ Tuning Binary All (Same URM)/RP3beta/RP3beta_O.py
@@ -1,21 +1,20 @@
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    from Recommenders.SLIM.SLIMElasticNetRecommender import MultiThreadSLIM_SLIMElasticNetRecommender, SLIMElasticNetRecommender
-    from Evaluation.K_Fold_Evaluator import K_Fold_Evaluator_MAP
-    from Utils.recsys2022DataReader import *
     from Evaluation.Evaluator import EvaluatorHoldout
-    import json
+    from Evaluation.K_Fold_Evaluator import K_Fold_Evaluator_MAP
     from datetime import datetime
+    from Utils.recsys2022DataReader import *
+    from Recommenders.GraphBased.RP3betaRecommender import RP3betaRecommender
     import optuna as op
-    import numpy as np
+    import json
     import csv
 
     # ---------------------------------------------------------------------------------------------------------
     # Creating CSV header
 
-    header = ['recommender', 'alpha', 'l1_ratio', 'TopK', 'MAP']
+    header = ['recommender', 'alpha', 'beta', 'TopK', 'MAP']
 
-    partialsFile = 'SlimElasticNet_' + datetime.now().strftime('%b%d_%H-%M-%S')
+    partialsFile = 'RP3beta_Normal' + datetime.now().strftime('%b%d_%H-%M-%S')
 
     with open('partials/' + partialsFile + '.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
@@ -25,10 +24,10 @@ if __name__ == "__main__":
 
     # ---------------------------------------------------------------------------------------------------------
     # Loading URMs
-    URM_train_init = load_BinURMTrainInit()
-    URM_train_list = load_1K_BinURMTrain()
-    URM_validation_list = load_1K_BinURMValid()
-    URM_test = load_BinURMTest()
+    URM_train_init = load_URMTrainInit()
+    URM_train_list = load_K_URMTrain()
+    URM_validation_list = load_K_URMValid()
+    URM_test = load_URMTest()
 
     evaluator_validation = K_Fold_Evaluator_MAP(URM_validation_list, cutoff_list=[10], verbose=False)
 
@@ -37,20 +36,21 @@ if __name__ == "__main__":
 
     def objective(trial):
 
-        recommender_SlimElasticnet_list = []
+        recommender_RP3beta_list = []
 
-        topK = trial.suggest_int("topK", 50, 300)
-        alpha = trial.suggest_float("alpha", 0.00001, 0.01)
-        l1_ratio = trial.suggest_float("l1_ratio", 0.00001, 0.01)
+        alpha = trial.suggest_float("alpha", 0.1, 0.9)
+        beta = trial.suggest_float("beta", 0.1, 0.9)
+        topK = trial.suggest_int("topK", 100, 1000)
 
         for index in range(len(URM_train_list)):
-            recommender_SlimElasticnet_list.append(SLIMElasticNetRecommender(URM_train_list[index]))
-            recommender_SlimElasticnet_list[index].fit(alpha=alpha, l1_ratio=l1_ratio, topK=topK)
 
-        MAP_result = evaluator_validation.evaluateRecommender(recommender_SlimElasticnet_list)
+            recommender_RP3beta_list.append(RP3betaRecommender(URM_train_list[index], verbose=False))
+            recommender_RP3beta_list[index].fit(alpha=alpha, topK=topK, beta=beta)
 
-        resultsToPrint = [recommender_SlimElasticnet_list[0].RECOMMENDER_NAME, alpha, l1_ratio, topK,
-                          sum(MAP_result) / len(MAP_result)]
+
+        MAP_result = evaluator_validation.evaluateRecommender(recommender_RP3beta_list)
+
+        resultsToPrint = [recommender_RP3beta_list[0].RECOMMENDER_NAME, alpha, beta, topK, sum(MAP_result) / len(MAP_result)]
 
         with open('partials/' + partialsFile + '.csv', 'a+', encoding='UTF8') as f:
             writer = csv.writer(f)
@@ -58,21 +58,22 @@ if __name__ == "__main__":
 
         return sum(MAP_result) / len(MAP_result)
 
+
     study = op.create_study(direction='maximize')
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective, n_trials=200)
 
     # ---------------------------------------------------------------------------------------------------------
     # Fitting and testing to get local MAP
 
     topK = study.best_params['topK']
     alpha = study.best_params['alpha']
-    l1_ratio = study.best_params['l1_ratio']
+    beta = study.best_params['beta']
 
-    recommender_SlimElasticNet = SLIMElasticNetRecommender(URM_train_init, verbose=False)
-    recommender_SlimElasticNet.fit(alpha=alpha, l1_ratio=l1_ratio, topK=topK)
+    recommender_RP3beta = RP3betaRecommender(URM_train_init, verbose=False)
+    recommender_RP3beta.fit(alpha=alpha, topK=topK, beta=beta)
 
     evaluator_test = EvaluatorHoldout(URM_test, cutoff_list=[10])
-    result_dict, _ = evaluator_test.evaluateRecommender(recommender_SlimElasticNet)
+    result_dict, _ = evaluator_test.evaluateRecommender(recommender_RP3beta)
 
     # ---------------------------------------------------------------------------------------------------------
     # Writing hyperparameter into a log
@@ -80,7 +81,7 @@ if __name__ == "__main__":
     resultParameters = result_dict.to_json(orient="records")
     parsed = json.loads(resultParameters)
 
-    with open("logs/" + recommender_SlimElasticNet.RECOMMENDER_NAME + "_logs_" + datetime.now().strftime(
+    with open("logs/" + recommender_RP3beta.RECOMMENDER_NAME + "_Normal_logs_" + datetime.now().strftime(
             '%b%d_%H-%M-%S') + ".json", 'w') as json_file:
         json.dump(study.best_params, json_file, indent=4)
         json.dump(parsed, json_file, indent=4)
