8b53c85af8062915c36dd9fc42a4bd1e83f40607
MichaelVitali <michael.vitali@mail.polimi.it>
2023-01-12 00:11:11 +0100

    tuning rp3

--- Tuning New URM/HT_RP3beta.py
+++ Tuning New URM/HT_RP3beta.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     from Data_manager.split_functions.split_train_validation_random_holdout import \
         split_train_in_two_percentage_global_sample
     import json
-    import csv
 
     # ---------------------------------------------------------------------------------------------------------
     # Loading URMs
